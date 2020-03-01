21c6a5b1028eadfb6de87b09518af3bf25fd1cf3
Alwin1847207 <alwin.joseph@mca.christuniversity.in>
2020-03-01 13:31:11 +0530

    initial commit

---
+++ app/src/main/java/com/example/bakerschoice/ui/slideshow/SlideshowViewModel.java
@@ -0,0 +1,19 @@
+package com.example.bakerschoice.ui.slideshow;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SlideshowViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public SlideshowViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is slideshow fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
