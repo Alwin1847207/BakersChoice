696874242743235e423fccaca01c01ceaa7627ff
jaygs8971 <50477871+jaygs8971@users.noreply.github.com>
2020-03-02 16:45:12 +0530

    jay commits

--- app/src/main/java/com/example/bakerschoice/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/bakerschoice/ui/home/HomeFragment.java
@@ -1,13 +1,11 @@
 package com.example.bakerschoice.ui.home;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -27,28 +25,28 @@ public class HomeFragment extends Fragment {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-<<<<<<< HEAD
-       // final TextView textView = root.findViewById(R.id.text_home);
-        final Button test=root.findViewById(R.id.test);
+
+        // final TextView textView = root.findViewById(R.id.text_home);
+        final Button test = root.findViewById(R.id.test);
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-               // textView.setText(s);
+                // textView.setText(s);
                 test.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        Intent i=new Intent(getActivity(), loginActivity.class);
+                        Intent i = new Intent(getActivity(), loginActivity.class);
                         startActivity(i);
                     }
                 });
-
-=======
-   //  final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-     //           textView.setText(s);
->>>>>>> d3e0183d6b66bc41c47cd00a0f87658b59e4d8e2
+//
+//   //  final TextView textView = root.findViewById(R.id.text_home);
+//        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//     //           textView.setText(s);
+//            }
+//        });
             }
         });
         return root;
